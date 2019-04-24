93d9c49a3584ad96db2b6b431c55ed08d0b8bc85
Apume <48521550+Apume@users.noreply.github.com>
2019-04-24 18:39:24 +0200

    la lista selecciona la cancion

--- app/src/main/java/com/example/adrian/proyectofinal/SonidosActivity.java
+++ app/src/main/java/com/example/adrian/proyectofinal/SonidosActivity.java
@@ -4,9 +4,11 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,9 +18,13 @@ public class SonidosActivity extends AppCompatActivity implements View.OnClickLi
     private Button bPause;
     private Button bStop;
     private MediaPlayer mediaplayer;
+    //
+    private MediaPlayer mp;
+    //
     private ListView lvSonidos;
     private ArrayList<String> ArraySonidos;
     private ArrayAdapter adaptador;
+    private String nombreCancion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +34,12 @@ public class SonidosActivity extends AppCompatActivity implements View.OnClickLi
         ArraySonidos = new ArrayList<>();
         ArraySonidos.add("caillou");
         ArraySonidos.add("lonely");
-
+        int [] resID= {R.raw.caillou,R.raw.lonely};
         adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,ArraySonidos);
         lvSonidos.setAdapter(adaptador);
 
         //Inicializamos la clase MediaPlayer asociandole el fichero de Audio
-        mediaplayer = MediaPlayer.create(this, R.raw.caillou);
+        //mediaplayer = MediaPlayer.create(this, R.raw.lonely);
 
         //Obtenemos los tres botones de la interfaz
         bPlay= findViewById(R.id.bPlay);
@@ -44,12 +50,43 @@ public class SonidosActivity extends AppCompatActivity implements View.OnClickLi
         bPlay.setOnClickListener(this);
         bStop.setOnClickListener(this);
         bPause.setOnClickListener(this);
-    }
+        //--------------------------------------------------------------------------------------------------------------------------------------
+        lvSonidos.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                // TODO Auto-generated method stub
+                String itemval = (String)lvSonidos.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Ha elegido " + itemval, Toast.LENGTH_SHORT).show();
+                nombreCancion = itemval;
+                /*if(position==0)
+                {
+                    nombreCancion = "caillou";
+                }
+                if(position==1)
+                {
+                   nombreCancion = "lonely";
+                }*/
+            }
+
+        });
+    }
+    //----------------------------------------------
+
+    //----------------------------------------------
     @Override
     public void onClick(View v) {
+        if(nombreCancion.equals("caillou"))
+        {
+            mediaplayer = MediaPlayer.create(this, R.raw.caillou);
+        }
+        if(nombreCancion.equals("lonely"))
+        {
+            mediaplayer = MediaPlayer.create(this, R.raw.lonely);
+        }
         switch(v.getId()){
             case R.id.bPlay:
+
                 //Iniciamos el audio
                 mediaplayer.start();
                 break;
@@ -66,10 +103,9 @@ public class SonidosActivity extends AppCompatActivity implements View.OnClickLi
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
                 break;
 
         }
+
     }
 }
