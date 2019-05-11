467d6692c728aecd84f28c7e9bd1728b6299794e
jairoCitores <47317050+jairoCitores@users.noreply.github.com>
2019-05-11 20:19:40 +0200

    Gestion de usuarios CORRECTO 100%

--- app/src/main/java/com/example/adrian/proyectofinal/PantallaAdmin.java
+++ app/src/main/java/com/example/adrian/proyectofinal/PantallaAdmin.java
@@ -1,5 +1,6 @@
 package com.example.adrian.proyectofinal;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,12 +8,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,39 +25,59 @@ import java.util.ArrayList;
 public class PantallaAdmin extends AppCompatActivity {
 
     private EditText etGesNombreUser, etGesPassUser;
-    private Button bBorrar, bModificar, bCerrarSesion;
+    private TextView tvUserSelected, tvPassSelected, tvToast;
+    private Button bBorrar, bModificar, bCancelar, bCerrarSesion;
     private AdminSQLiteOpenHelper admin;
     private SQLiteDatabase db;
     private ArrayList<String> ArrayUsuarios;
     private ArrayAdapter adaptador;
     private ListView lvUsuarios;
     private String userSelected, passSelected;
-
+    private Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_admin);
 
+        // Toast personalizado
+        toast = new Toast(getApplicationContext());
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.layout_toast,
+                (ViewGroup) findViewById(R.id.lytToast));
+
+        tvToast = layout.findViewById(R.id.tvToast);
+        toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+
+
+
+
+        // Declaracion de los componentes -------------------------------
         lvUsuarios = findViewById(R.id.lvUsuarios);
+        tvUserSelected = findViewById(R.id.tvUserSelected);
+        tvPassSelected = findViewById(R.id.tvPassSelected);
         etGesNombreUser = findViewById(R.id.etGesNombreUser);
         etGesPassUser = findViewById(R.id.etGesPassUser);
         bBorrar = findViewById(R.id.bBorrar);
         bModificar = findViewById(R.id.bModificar);
+        bCancelar = findViewById(R.id.bCancelar);
         bCerrarSesion = findViewById(R.id.bCerrarSesion);
 
+        tvUserSelected.setVisibility(View.INVISIBLE);
+        tvPassSelected.setVisibility(View.INVISIBLE);
         etGesNombreUser.setVisibility(View.INVISIBLE);
         etGesPassUser.setVisibility(View.INVISIBLE);
         bBorrar.setVisibility(View.INVISIBLE);
         bModificar.setVisibility(View.INVISIBLE);
-        bCerrarSesion.setVisibility(View.INVISIBLE);
-
-
-        //admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        bCancelar.setVisibility(View.INVISIBLE);
+        //---------------------------------------------------------------
 
         justDoIt();
 
 
-        //En este metodo asignamos a los items del ListView
+        //En este metodo asignamos a los items del ListView su funcion al hacer click en ellos
         lvUsuarios.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
@@ -60,19 +85,22 @@ public class PantallaAdmin extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 String itemSelected = lvUsuarios.getItemAtPosition(position).toString().split(" ")[1];
 
+                tvUserSelected.setVisibility(View.VISIBLE);
+                tvPassSelected.setVisibility(View.VISIBLE);
                 etGesNombreUser.setVisibility(View.VISIBLE);
                 etGesPassUser.setVisibility(View.VISIBLE);
                 bBorrar.setVisibility(View.VISIBLE);
                 bModificar.setVisibility(View.VISIBLE);
-                bCerrarSesion.setVisibility(View.VISIBLE);
-
-                etGesNombreUser.setText(selectBD(itemSelected).get(0));
-                etGesPassUser.setText(selectBD(itemSelected).get(1));
+                bCancelar.setVisibility(View.VISIBLE);
 
                 userSelected = selectBD(itemSelected).get(0);
                 passSelected = selectBD(itemSelected).get(1);
 
-                Toast.makeText(getApplicationContext(), "Usuario "+itemSelected+" seleccionado", Toast.LENGTH_SHORT).show();
+                tvUserSelected.setText("Modificando el usuario \n➤ "+userSelected);
+                tvPassSelected.setText("Contraseña actual \n➤ "+passSelected);
+                etGesNombreUser.setText(userSelected);
+                etGesPassUser.setText(passSelected);
+
             }
         });
 
@@ -80,12 +108,14 @@ public class PantallaAdmin extends AppCompatActivity {
 
     public void borrar(View v){
         String nombreUser = etGesNombreUser.getText().toString();
+
         if(!TextUtils.isEmpty(nombreUser)){
             AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
             SQLiteDatabase db = admin.getWritableDatabase();
 
             if(db.delete("usuarios","nombre ="+"'"+nombreUser+"'", null)==1){
                 Toast.makeText(this, "Usuario eliminado", Toast.LENGTH_SHORT).show();
+                limpiarVentana();
                 etGesNombreUser.setText("");
                 etGesPassUser.setText("");
             }else{
@@ -108,35 +138,33 @@ public class PantallaAdmin extends AppCompatActivity {
     public void modificar(View v){
         String nombreUser = etGesNombreUser.getText().toString();
         String passUser = etGesPassUser.getText().toString();
-        if(!TextUtils.isEmpty(nombreUser)){
 
-            db.execSQL("UPDATE usuarios SET nombre='"+nombreUser+"', clave='"+passUser+"' WHERE nombre= '"+userSelected+"'");
+        if(!TextUtils.isEmpty(nombreUser) && !TextUtils.isEmpty(passUser)){
+            ContentValues valores = new ContentValues();
+            valores.put("nombre",nombreUser);
+            valores.put("clave", passUser);
+
+            db.update("usuarios", valores, "nombre='"+userSelected+"'", null);
+            tvToast.setText(" Usuario "+userSelected+" actualizado");
+            toast.show();
+            limpiarVentana();
+            //db.execSQL("UPDATE usuarios SET nombre='"+nombreUser+"', clave='"+passUser+"' WHERE nombre= '"+userSelected+"'");
 
         }else{
 
             if(TextUtils.isEmpty(nombreUser)) {
+                etGesNombreUser.requestFocus();
                 etGesNombreUser.setError("Introduce un usuario");
             }
             if(TextUtils.isEmpty(passUser)){
                 etGesPassUser.setError("Introduce una contraseña");
             }
-
-
         }
-
-
-
     }
 
     public void cancelar(View v){
-        etGesNombreUser.setText("");
-        etGesPassUser.setText("");
+        limpiarVentana();
 
-        etGesNombreUser.setVisibility(View.INVISIBLE);
-        etGesPassUser.setVisibility(View.INVISIBLE);
-        bBorrar.setVisibility(View.INVISIBLE);
-        bModificar.setVisibility(View.INVISIBLE);
-        bCerrarSesion.setVisibility(View.INVISIBLE);
     }
 
     public void cerrarSesion(View v){
@@ -192,5 +220,19 @@ public class PantallaAdmin extends AppCompatActivity {
         return datosUser;
     }
 
+    public void limpiarVentana(){
+        tvUserSelected.setText("");
+        tvPassSelected.setText("");
+        etGesNombreUser.setText("");
+        etGesPassUser.setText("");
+
+        tvUserSelected.setVisibility(View.INVISIBLE);
+        tvPassSelected.setVisibility(View.INVISIBLE);
+        etGesNombreUser.setVisibility(View.INVISIBLE);
+        etGesPassUser.setVisibility(View.INVISIBLE);
+        bBorrar.setVisibility(View.INVISIBLE);
+        bModificar.setVisibility(View.INVISIBLE);
+        bCancelar.setVisibility(View.INVISIBLE);
+    }
 
 }
