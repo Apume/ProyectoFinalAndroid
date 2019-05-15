8f81afae4a79a2e4ef8ea9d63783c97054f45958
Apume <48521550+Apume@users.noreply.github.com>
2019-05-15 18:44:03 +0200

    Comprobacion de campos, botones de confirmar, borrar y cancelar hechos en teoria. Revisar.

--- app/src/main/java/com/example/adrian/proyectofinal/EditorNotasActivity.java
+++ app/src/main/java/com/example/adrian/proyectofinal/EditorNotasActivity.java
@@ -1,11 +1,14 @@
 package com.example.adrian.proyectofinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditorNotasActivity extends AppCompatActivity {
     private EditText etTitulo,contenidoNota;
+    private String elTitulo, elContenido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +19,41 @@ public class EditorNotasActivity extends AppCompatActivity {
         String elTitulo = getIntent().getStringExtra("tituloNota");
         String elContenido = getIntent().getStringExtra("contenidoNota");
 
-    }
-
-    public void modificar(){
+        comprobarCampos(elTitulo, elContenido);
 
     }
 
-    public void borrar(){
+    public void modificar(View v){
+        Intent i = new Intent(this, NotasActivity.class);
+        elTitulo = etTitulo.getText().toString();
+        elContenido = contenidoNota.getText().toString();
+        i.putExtra("titulo",elTitulo);
+        i.putExtra("contenido", elContenido);
+        startActivity(i);
 
     }
 
+    public void borrar(View v){
+        etTitulo.setText(null);
+        contenidoNota.setText(null);
+    }
+
+    public void cancelar(View v){
+        Intent i = new Intent(this, NotasActivity.class);
+        startActivity(i);
+    }
+
+    public void comprobarCampos(String elTitulo, String elContenido){
+        if(elTitulo == null){
+            etTitulo.setText("No hay titulo");
+        }else{
+            etTitulo.setText(elTitulo);
+        }
+
+        if(elContenido == null){
+            contenidoNota.setText("No hay contenido");
+        }else{
+            contenidoNota.setText(elContenido);
+        }
+    }
 }
